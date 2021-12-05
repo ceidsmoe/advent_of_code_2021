966a18581cd3aab78514a1d85175e9d7953c122d
Chris Eidsmoe <chris.eidsmoe@gmail.com>
2021-12-04 21:04:05 -0600

    fixed day 3 p2

--- day3/part2.py
+++ day3/part2.py
@@ -1,19 +1,49 @@
 if __name__ == "__main__":
     with open("input") as f:
-        ones = [0] * 12
-        num_lines = 0
+        binary_strings = []
         for line in f.readlines():
-            num_lines += 1
-            for i, bit in enumerate(line):
-                if bit == '1':
-                    ones[i] += 1
+            binary_strings.append(line)
         
-        oxygen = 0
-        co2 = 0
-        for i, bit in enumerate(ones):
-            if bit >= (num_lines/2):
-                oxygen |= (1 << (11 - i))
-            else:
-                co2 |= (1 << (11 - i))
+        # do o2 first
+        old_array = list(binary_strings)
+        new_array = [[], []]
+        bit_count = 0
         
+        while len(old_array) > 1:
+            for line in old_array:
+                if line[bit_count] == '0':
+                    new_array[0].append(line)
+                else:
+                    new_array[1].append(line)
+            if len(new_array[1]) >= len(new_array[0]):
+                old_array = new_array[1]
+            else: 
+                old_array = new_array[0]
+            
+            bit_count += 1
+            new_array = [[], []]
+        
+        oxygen = int(old_array[0], 2)
+
+        # now do co2
+        old_array = list(binary_strings)
+        new_array = [[], []]
+        bit_count = 0
+        
+        while len(old_array) > 1:
+            for line in old_array:
+                if line[bit_count] == '0':
+                    new_array[0].append(line)
+                else:
+                    new_array[1].append(line)
+            if len(new_array[1]) >= len(new_array[0]):
+                old_array = new_array[0]
+            else: 
+                old_array = new_array[1]
+            
+            bit_count += 1
+            new_array = [[], []]
+
+        co2 = int(old_array[0], 2)
+
         print(oxygen, co2, oxygen * co2)
